b0c343b7ed0626ea7e2f824f624b752b97ba4fff
Semhal Desta <190836816+SemhalDesta@users.noreply.github.com>
2026-06-26 18:53:07 +0400

    Initial project setup - Android + Firebase dependencies

---
+++ app/src/main/java/sosina/terefe/adu/ac/ae/sawt/MainActivity.java
@@ -0,0 +1,19 @@
+package sosina.terefe.adu.ac.ae.sawt;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new LoginFragment())
+                    .commit();
+        }
+    }
+}
